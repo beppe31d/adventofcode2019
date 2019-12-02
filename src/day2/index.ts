50c73b163bf6faeb45c9c065c81b521ffdaa3a4b
Giuseppe Del Gaudio <beppe31@gmail.com>
2019-12-02 13:48:45 +0100

    Day 2 part 2

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -3,10 +3,23 @@ import { OpCode } from './OpCode';
 const opCode = new OpCode();
 const fs = require("fs");
 const file = fs.readFileSync("src/day2/input.txt");
-let states = file.toString().split(",").map((value: string) => parseInt(value));
+let initialStates = file.toString().split(",").map((value: string) => parseInt(value));
 
+let states = initialStates.map((item) => item);
 states[1] = 12;
 states[2] = 2;
 let result = opCode.calculate(states);
 
 console.log(result[0]);
+
+for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+        states = initialStates.map((item) => item);
+        states[1] = noun;
+        states[2] = verb;
+        result = opCode.calculate(states);
+        if (result !== undefined && result[0] === 19690720) {
+            console.log (noun, verb);
+        }
+    }
+}
